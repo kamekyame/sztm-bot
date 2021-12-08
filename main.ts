81968a2e0a98a1f111b186777874b4472410dc9d
Suzu Tomo <kamekyame@outlook.com>
2021-12-08 11:44:03 +0900

    fix

--- main.ts
+++ main.ts
@@ -5,4 +5,4 @@ const port = Deno.args[0];
 import { serve } from "https://deno.land/std@0.117.0/http/server.ts";
 
 console.log(`http://localhost:${port}/`);
-serve((req) => new Response("Hello World\n"), { addr: `:${port}` });
+serve((req) => new Response("Hello World!!!!!!\n"), { addr: `:${port}` });

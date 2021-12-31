0ecaa5d2250d3473bfd2b79fe2a971a039aaed0c
Suzu Tomo <kamekyame@outlook.com>
2021-12-31 19:53:40 +0900

    fix(cron): delete mydns_update

--- src/cron.ts
+++ src/cron.ts
@@ -2,10 +2,6 @@ import { Cron } from "./deps.ts";
 import { cronOptions } from "./util.ts";
 
 import { roboconInfoTweet } from "./cron/robocon_info.ts";
-import { mydnsUpdateDNS } from "./cron/mydns_update.ts";
-
-// 毎日0時にDNS情報をアップデート
-new Cron("0 0 0 * * *", cronOptions, mydnsUpdateDNS);
 
 // 毎日7時にロボコン情報をツイート
 new Cron("0 0 7 * * *", cronOptions, roboconInfoTweet);

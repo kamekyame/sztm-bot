028863914b03ecf45df55a5a5aebf530587f878e
Suzu Tomo <kamekyame@outlook.com>
2022-01-04 19:11:57 +0900

    feat(api): version APIを追加

--- src/api.ts
+++ src/api.ts
@@ -1,8 +1,12 @@
-import { Application } from "./deps.ts";
+import { Application, Router, Status } from "./deps.ts";
+import { reqEnv } from "./env.ts";
 import { logger, responseTypeHeader } from "./api/middleware.ts";
 
-// Read Arguments
-const port = parseInt(Deno.args[0]) || 8888;
+const router = new Router();
+router.get("/version", (ctx) => {
+  ctx.response.status = Status.OK;
+  ctx.response.body = reqEnv.HEROKU_RELEASE_VERSION;
+});
 
 // Oak application
 const app = new Application();
@@ -10,12 +14,14 @@ const app = new Application();
 app.use(logger);
 app.use(responseTypeHeader);
 
-// Hello World!
+app.use(router.routes());
 app.use((ctx) => {
-  ctx.response.body = "Hello World!";
+  ctx.response.status = Status.NotFound;
+  ctx.response.body = { message: "Not Found" };
 });
-
-console.log(`Listen to http://localhost:${port}/`);
+// Read Arguments
+const port = parseInt(Deno.args[0]) || 8888;
 app.listen({ port }).then(() => {
   Deno.exit(1);
 });
+console.log(`Listen to http://localhost:${port}/`);

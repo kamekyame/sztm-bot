cf45c40972ad19cf9991f0038a39533ad5a9f303
Suzu Tomo <kamekyame@outlook.com>
2022-04-03 19:38:04 +0900

    fix: bump twitter_api_client to v0.2.2 from v0.2.1

--- src/deps.ts
+++ src/deps.ts
@@ -6,8 +6,7 @@ export * as datetime from "https://deno.land/std@0.119.0/datetime/mod.ts";
 // Third Party Modules
 export * from "https://deno.land/x/dotenv@v3.1.0/mod.ts";
 export * from "https://deno.land/x/oak@v10.1.0/mod.ts";
-// export * from "https://deno.land/x/twitter_api_client@v0.2.1/mod.ts";
-export * from "https://cdn.jsdelivr.net/gh/kamekyame/twitter_api_client@f97304bf29c817e6f8441a66d8988776349295ab/mod.ts";
+export * from "https://deno.land/x/twitter_api_client@v0.2.2/mod.ts";
 export { oakCors } from "https://deno.land/x/cors@v1.2.2/mod.ts";
 
 export * from "https://cdn.jsdelivr.net/gh/hexagon/croner@4/src/croner.js";
@@ -18,5 +17,3 @@ export * from "https://www.gstatic.com/firebasejs/9.6.0/firebase-app.js";
 export * from "https://www.gstatic.com/firebasejs/9.6.0/firebase-auth.js";
 // @deno-types="https://cdn.esm.sh/v61/firebase@9.6.0/app/dist/firestore/index.d.ts"
 export * as firestore from "https://www.gstatic.com/firebasejs/9.6.0/firebase-firestore.js";
-
-export * from "https://kamekyame.github.io/deno_tools/http/mod.ts";

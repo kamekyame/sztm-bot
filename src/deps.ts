2915a49769eb3b18c6d30dcc9f20bf3a2f56b74d
denopendabot <denopendabot@gmail.com>
2022-10-25 09:52:22 +0000

    build(deps): bump deno.land/std from 0.159.0 to 0.160.0

--- src/deps.ts
+++ src/deps.ts
@@ -1,7 +1,7 @@
 // Standard Library
 export { parse as yamlParse } from "https://deno.land/std@0.159.0/encoding/yaml.ts";
 export * as Colors from "https://deno.land/std@0.159.0/fmt/colors.ts";
-export * as datetime from "https://deno.land/std@0.159.0/datetime/mod.ts";
+export * as datetime from "https://deno.land/std@0.160.0/datetime/mod.ts";
 
 // Third Party Modules
 export * from "https://deno.land/x/dotenv@v3.2.0/mod.ts";

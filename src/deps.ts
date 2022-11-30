0d1ce13a9bbe9ee19c8c696faa3c6de7ea8b2ed8
SuzuTomo <47011206+kamekyame@users.noreply.github.com>
2022-11-30 06:40:15 +0000

    fix(stream/maji-uranai-collect): タイムゾーンの影響で保存する際に日付が変わらないように修正

--- src/deps.ts
+++ src/deps.ts
@@ -1,7 +1,6 @@
 // Standard Library
 export { parse as yamlParse } from "https://deno.land/std@0.162.0/encoding/yaml.ts";
 export * as Colors from "https://deno.land/std@0.162.0/fmt/colors.ts";
-export * as datetime from "https://deno.land/std@0.162.0/datetime/mod.ts";
 
 // Third Party Modules
 export * from "https://deno.land/x/dotenv@v3.2.0/mod.ts";

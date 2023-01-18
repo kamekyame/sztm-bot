f5bd3f2c0cd3d7b90721f13ebdbfb60730c3282d
SuzuTomo <47011206+kamekyame@users.noreply.github.com>
2023-01-18 15:41:19 +0900

    fix: update firebase to 9.15.0 (#110)

--- src/deps.ts
+++ src/deps.ts
@@ -11,8 +11,8 @@ export * as ptera from "https://deno.land/x/ptera@v1.0.2/mod.ts";
 
 export * from "https://cdn.jsdelivr.net/gh/hexagon/croner@4/src/croner.js";
 
-export * as app from "https://esm.sh/firebase@9.14.0/app";
-export * as auth from "https://esm.sh/firebase@9.14.0/auth";
-export * as firestore from "https://esm.sh/firebase@9.14.0/firestore";
+export * as app from "https://esm.sh/firebase@9.15.0/app";
+export * as auth from "https://esm.sh/firebase@9.15.0/auth";
+export * as firestore from "https://esm.sh/firebase@9.15.0/firestore";
 
 export { TwitterApi } from "npm:twitter-api-v2@1.14.0";

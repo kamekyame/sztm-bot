6ca1df043d759a05ae92542a2a82829f519dd825
denopendabot <denopendabot@github.com>
2023-01-18 00:31:13 +0000

    build(deps): bump esm.sh/firebase from 9.14.0 to 9.15.0

--- src/deps.ts
+++ src/deps.ts
@@ -1,6 +1,6 @@
 // Standard Library
-export { parse as yamlParse } from "https://deno.land/std@0.173.0/encoding/yaml.ts";
-export * as Colors from "https://deno.land/std@0.173.0/fmt/colors.ts";
+export { parse as yamlParse } from "https://deno.land/std@0.162.0/encoding/yaml.ts";
+export * as Colors from "https://deno.land/std@0.162.0/fmt/colors.ts";
 
 // Third Party Modules
 export * from "https://deno.land/x/dotenv@v3.2.0/mod.ts";
@@ -11,6 +11,6 @@ export * as ptera from "https://deno.land/x/ptera@v1.0.2/mod.ts";
 
 export * from "https://cdn.jsdelivr.net/gh/hexagon/croner@4/src/croner.js";
 
-export * as app from "https://esm.sh/firebase@9.14.0/app";
-export * as auth from "https://esm.sh/firebase@9.14.0/auth";
-export * as firestore from "https://esm.sh/firebase@9.14.0/firestore";
+export * as app from "https://esm.sh/firebase@9.15.0/app";
+export * as auth from "https://esm.sh/firebase@9.15.0/auth";
+export * as firestore from "https://esm.sh/firebase@9.15.0/firestore";

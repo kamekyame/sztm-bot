4b91ac81a9af632080e88892542a27dba7dabda3
Suzu Tomo <kamekyame@outlook.com>
2023-05-30 00:49:21 +0900

    chore: 依存関係をアップデート

--- src/deps.ts
+++ src/deps.ts
@@ -1,11 +1,10 @@
 // Standard Library
-export { parse as yamlParse } from "https://deno.land/std@0.162.0/encoding/yaml.ts";
-export * as Colors from "https://deno.land/std@0.162.0/fmt/colors.ts";
+export { parse as yamlParse } from "https://deno.land/std@0.189.0/yaml/mod.ts";
+export * as Colors from "https://deno.land/std@0.189.0/fmt/colors.ts";
+export { load } from "https://deno.land/std@0.189.0/dotenv/mod.ts";
 
 // Third Party Modules
-export * from "https://deno.land/x/dotenv@v3.2.0/mod.ts";
-export * from "https://deno.land/x/oak@v11.1.0/mod.ts";
-export * from "https://deno.land/x/twitter_api_client@v0.3.2/mod.ts";
+export * from "https://deno.land/x/oak@v12.5.0/mod.ts";
 export { oakCors } from "https://deno.land/x/cors@v1.2.2/mod.ts";
 export * as ptera from "https://deno.land/x/ptera@v1.0.2/mod.ts";
 
@@ -18,4 +17,4 @@ export * as auth from "https://www.gstatic.com/firebasejs/9.19.0/firebase-auth.j
 // @deno-types="https://esm.sh/firebase@9.15.0/firestore"
 export * as firestore from "https://www.gstatic.com/firebasejs/9.19.0/firebase-firestore.js";
 
-export { TwitterApi } from "npm:twitter-api-v2@1.14.0";
+export { TwitterApi } from "npm:twitter-api-v2@1.14.3";

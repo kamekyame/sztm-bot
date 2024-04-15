927c8b35178ba576eaa123ff5f954283ee6286c7
Suzu Tomo <kamekyame@outlook.com>
2024-04-15 21:56:25 +0900

    ✨ [cron] ロボコン2024日程を追加 & Deno.cronを使用するように変更

--- src/deps.ts
+++ src/deps.ts
@@ -1,6 +1,3 @@
-// // Third Party Modules
-export * from "https://cdn.jsdelivr.net/gh/hexagon/croner@4/src/croner.js";
-
 // @deno-types="https://esm.sh/firebase@9.15.0/app"
 export * as app from "https://www.gstatic.com/firebasejs/9.19.0/firebase-app.js";
 // @deno-types="https://esm.sh/firebase@9.15.0/auth"

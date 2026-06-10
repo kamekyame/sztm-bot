e6f58256bd6a78f2dc356ee37c94fe5da9d2fed2
kamekyame <kamekyame@outlook.com>
2026-06-11 00:16:25 +0900

    古いDenoDeploy用にCronをコメントアウト

--- src/cron.ts
+++ src/cron.ts
@@ -1,7 +1,7 @@
-import { roboconInfoTweet } from "./cron/robocon_info.ts";
-import { t7sInfoTweet } from "./cron/t7s_info.ts";
+// import { roboconInfoTweet } from "./cron/robocon_info.ts";
+// import { t7sInfoTweet } from "./cron/t7s_info.ts";
 
 // 毎日7時(UTCで22時)にロボコン情報をツイート
-Deno.cron("Robocon Info Post", "0 22 * * *", roboconInfoTweet);
+// Deno.cron("Robocon Info Post", "0 22 * * *", roboconInfoTweet);
 // 毎日7時30分(UTCで22時30分)にナナシス周年情報をツイート
-Deno.cron("t7s Info Post", "30 22 * * *", t7sInfoTweet);
+// Deno.cron("t7s Info Post", "30 22 * * *", t7sInfoTweet);
